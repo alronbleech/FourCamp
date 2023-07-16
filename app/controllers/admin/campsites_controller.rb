a5258df5d2b6b07cfe85b2d45e2603122913e277
alronbleech <126852030+alronbleech@users.noreply.github.com>
2023-07-16 05:13:38 +0000

    [Fix]

--- app/controllers/admin/campsites_controller.rb
+++ app/controllers/admin/campsites_controller.rb
@@ -8,14 +8,30 @@ class Admin::CampsitesController < ApplicationController
   def index
     @prefectures = Prefecture.all
     @campsite_types = CampsiteType.all
-    if params[:prefecture_id].present?
+    if params[:prefecture_id].present? && params[:campsite_type_id].present?
       @campsites = Campsite.where(prefecture_id: params[:prefecture_id], campsite_type_id: params[:campsite_type_id]).page(params[:page]).per(5)
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    elsif params[:prefecture_id].present?
+      @campsites = Campsite.where(prefecture_id: params[:prefecture_id]).page(params[:page]).per(5)
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    elsif params[:campsite_type_id].present?
+      @campsites = Campsite.where(prefecture_id: params[:campsite_type_id]).page(params[:page]).per(5)
       respond_to do |format|
         format.html
         format.js
       end
     else
       @campsites = Campsite.page(params[:page]).per(5)
+      respond_to do |format|
+        format.html
+        format.js
+      end
     end
   end
 
